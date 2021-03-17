dee45760d90e7dfdb1f948f3212084e6d426d9f4
Tech29-sam <saumyap2929@gmail.com>
2021-03-17 17:03:04 +0530

    AR

---
+++ ACM_BusinessCard/Assets/Scripts/CameraFocusController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour
+{
+
+    private bool mVuforiaStarted = false;
+
+    void Start()
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+
+        if (vuforia != null)
+            vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+    }
+
+    private void StartAfterVuforia()
+    {
+        mVuforiaStarted = true;
+        SetAutofocus();
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            // App resumed
+            if (mVuforiaStarted)
+            {
+                SetAutofocus(); 
+            }
+        }
+    }
+
+    private void SetAutofocus()
+    {
+        if (CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO))
+        {
+            Debug.Log("Autofocus set");
+        }
+        else
+        {
+            Debug.Log("this device doesn't support auto focus");
+        }
+    }
+}
